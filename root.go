beaa94cf793f06922c3497c1d42e3b04a2a96f92
osad-sakana <mezzoforte.mackerel@icloud.com>
2025-02-01 17:31:43 +0900

    CLIの骨格を作成

---
+++ root.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "groovepush",
+	Short: "A simple CLI to locally version-control your music project files"
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
